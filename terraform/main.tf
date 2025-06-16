7e772e1e2635c1d62f374e5231650356e1a51f2b
Providence-ops <providence.asemah@betika.com>
2025-06-16 23:43:30 +0000

    Updated main.tf
    main.tf update

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c02fb55956c7d316"
+  ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
   key_name      = var.key_name
   tags = {
